457dacb3aed5c2406dc07212a5643b33844da72c
SyedJaved-12b6 <syedjavedsj123@gmail.com>
2025-08-06 14:19:24 +0530

    Terraform Dynamic Expressions

---
+++ terraform dynamic expression/main.tf
@@ -0,0 +1,34 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "syedjaved-rg"
+  location = "westus2"
+}
+
+# Create Network Security Group
+resource "azurerm_network_security_group" "example" {
+  name                = "syedjaved-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  # Here's where we need the dynamic block
+  dynamic "security_rule" {
+    for_each = local.nsg_rules
+    content {
+      name                       = security_rule.key
+      priority                   = security_rule.value.priority
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range         = "*"
+      destination_port_range    = security_rule.value.destination_port_range
+      source_address_prefix     = "*"
+      destination_address_prefix = "*"
+      description               = security_rule.value.description
+    }
+  }
+}
+
+# Output the security rules
+output "security_rules" {
+  value = azurerm_network_security_group.example.security_rule
+}  
+

c850ef60efd85d7e93839471d9fd94be7faced16
SyedJaved-12b6 <syedjavedsj123@gmail.com>
2025-08-06 18:41:12 +0530

    NSG Rules Functions

---
+++ Network Security Group Rules/variables.tf
@@ -0,0 +1,31 @@
+variable "project_name" {
+  type = string
+  description = "project name"
+  default = "Project ALPHA Resources"
+}
+
+variable "default_tags" {
+   type = map(string)
+   default = {
+     "company" = "TechCorp"
+     "managed_by" = "terraform"
+   }
+}
+
+variable "environment_tags" {
+  type = map(string)
+  default = {
+    "environment" = "production"
+    "cost_center" = "cc-123"
+  }
+}
+
+variable "storage_account_name" {
+  type = string
+  default = "syedjstutoriaLS with!javedthis is should be formatted"
+}
+
+variable "allowed_ports" {
+  type = string
+  default = "80,443,3306"
+}
